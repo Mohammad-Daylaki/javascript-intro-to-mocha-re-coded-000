7ae948527196376c993c45fe9d8f14f5e96b78a6
Mohammad Daylaki <github email address>
2017-02-20 13:43:45 +0300

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+ function favoriteIceCream(flavor) {
+  //debugger
+   return `I love ${flavor}`
+ }
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+ function roundDown(number) {
+   return Math.floor(number);
+ }
+
+function theTruth() {
+  return true;
+}
